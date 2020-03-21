2b1c7b67c07d63e11b23313fa1ac6de44c2c7557
João Pedro Braz <joao.braz@ekaizen-br.com>
2020-03-20 23:22:42 -0300

    Aprimorado método de validar CPF

--- src/main/java/com/mycompany/exerciciofurgeri/Exercicio.java
+++ src/main/java/com/mycompany/exerciciofurgeri/Exercicio.java
@@ -14,13 +14,13 @@ import java.util.StringTokenizer;
  * @author joao
  */
 public class Exercicio {
-    
+
     public void printLetters(String word) {
         for (int i = 0; i < word.length(); i++) {
             System.out.println(word.charAt(i));
         }
     }
-    
+
     public void printMultiplicationTable(Integer number) {
         if (number < 1 || number > 10) {
             System.out.println("Número Inválido");
@@ -30,16 +30,16 @@ public class Exercicio {
             System.out.println(number * i);
         }
     }
-    
+
     public void printFiveRandomNumbersBetweenOneAndOneThousand() {
         final Integer max = 1000, min = 1;
-        
+
         for (int i = 0; i < 5; i++) {
             Integer randomInteger = (int) (Math.random() * ((max - min) + 1)) + min;
             System.out.println(randomInteger);
         }
     }
-    
+
     public void printDayOfWeek(Integer number) {
         String dayOfWeek = "Inexistente";
         switch (number) {
@@ -65,46 +65,46 @@ public class Exercicio {
                 dayOfWeek = "Sábado";
                 break;
         }
-        
+
         System.out.println(dayOfWeek);
     }
-    
+
     public void printNames(List<String> names) {
         names.forEach((name) -> {
             System.out.println(name);
         });
     }
-    
+
     public Integer getEvenNumbers(List<Integer> numbers) {
         return (int) numbers.stream().filter((number) -> (number % 2 == 0)).count();
     }
-    
+
     public Integer sumElements(Integer[] numbers) {
         Integer sum = 0;
-        
+
         for (Integer number : numbers) {
             sum += number;
         }
-        
+
         return sum;
     }
-    
+
     public Double getArithmeticAverage(List<Double> grades) {
         Double sum = 0d;
         return grades.stream().reduce(sum, Double::sum) / grades.size();
     }
-    
+
     public Integer[] getIntegersBetween(Integer amount, Integer endPoint) {
         Integer[] numbers = new Integer[amount];
         for (int i = 0; i < amount; i++) {
             numbers[i] = (int) (Math.random() * ((endPoint) + 1));
         }
-        
+
         return numbers;
     }
-    
+
     public Boolean validateCPF(String CPF) {
         String cpfPattern = "([0-9]{3}\\.){2}[0-9]{3}\\/[0-9]{2}";
-        return CPF.replaceAll("\\D", "").length() == 11 && CPF.matches(cpfPattern);
+        return CPF.matches(cpfPattern);
     }
 }
